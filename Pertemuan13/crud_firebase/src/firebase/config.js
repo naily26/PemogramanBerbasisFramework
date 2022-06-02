51885eb565dd8e1c24352e5159bb9d6ccf1ce2fa
naily26 <nailymasruri@gmail.com>
2022-06-02 14:38:43 +0700

    update

---
+++ Pertemuan13/crud_firebase/src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB6vqONWg1-5aTM6O4eyYanZ6oQSyfoZMs",
+    authDomain: "crud-7fb31.firebaseapp.com",
+    projectId: "crud-7fb31",
+    storageBucket: "crud-7fb31.appspot.com",
+    messagingSenderId: "926569592714",
+    appId: "1:926569592714:web:00a1698b53d65bc6337a73"
+};
+export default firebaseConfig;
